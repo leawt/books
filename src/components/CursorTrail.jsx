8237233d345c789ba205093f6d2e5960bdea2285
leawt <100747924+leawt@users.noreply.github.com>
2025-12-24 08:02:45 -0500

    Revert to default browser cursors

--- src/components/CursorTrail.jsx
+++ src/components/CursorTrail.jsx
@@ -2,38 +2,9 @@ import { useEffect } from 'react';
 
 const CursorTrail = () => {
   useEffect(() => {
-    let trails = [];
-    const maxTrails = 8;
-
-    const createTrail = (e) => {
-      const trail = document.createElement('div');
-      trail.className = 'cursor-trail';
-      trail.style.left = `${e.clientX}px`;
-      trail.style.top = `${e.clientY}px`;
-      document.body.appendChild(trail);
-      trails.push(trail);
-
-      // Remove old trails
-      if (trails.length > maxTrails) {
-        const oldTrail = trails.shift();
-        if (oldTrail) {
-          oldTrail.remove();
-        }
-      }
-
-      // Remove trail after animation
-      setTimeout(() => {
-        trail.remove();
-        trails = trails.filter(t => t !== trail);
-      }, 500);
-    };
-
-    window.addEventListener('mousemove', createTrail);
-
-    return () => {
-      window.removeEventListener('mousemove', createTrail);
-      trails.forEach(trail => trail.remove());
-    };
+    // Disable the sparkle trail since we're using retro cursors now
+    // The retro cursors themselves provide the nostalgic feel
+    return () => {};
   }, []);
 
   return null;
